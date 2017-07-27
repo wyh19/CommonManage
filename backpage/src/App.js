0aa32f77dbacd098a24e471569974db794c1a2cf
wyh <wyh_19@163.com>
2017-07-27 21:58:03 +0800

    添加框架文件

--- backpage/src/App.js
+++ backpage/src/App.js
@@ -1,20 +1,21 @@
 import React, {Component} from 'react';
-import {Layout ,Menu, Icon} from 'antd';
+import {Layout, Menu, Icon} from 'antd';
 import './App.css';
 import {Link} from 'react-router';
 
 const {SubMenu} = Menu;
-const {Header,Sider, Content, Footer} = Layout;
+const {Header, Sider, Content, Footer} = Layout;
 
 class App extends Component {
-    state={
-        collapsed:false
+    state = {
+        collapsed: false
     };
     toggle = () => {
         this.setState({
             collapsed: !this.state.collapsed,
         });
     };
+
     render() {
         return (
             <Layout>
@@ -23,42 +24,44 @@ class App extends Component {
                     collapsible
                     collapsed={this.state.collapsed}
                 >
-                    <div className="logo" />
+                    <div className="logo"/>
                     <Menu theme="dark" mode="inline" defaultSelectedKeys={['/app/ui/buttons']}>
                         <SubMenu
                             key="/app/ui"
-                            title={<span><Icon type="scan" /><span className="nav-text">UI</span></span>}
+                            title={<span><Icon type="scan"/><span className="nav-text">UI</span></span>}
                         >
                             <Menu.Item key="/app/ui/buttons"><Link to={'/app/ui/buttons'}>按钮</Link></Menu.Item>
                             <Menu.Item key="/app/ui/grids"><Link to={'/app/ui/grids'}>栅格</Link></Menu.Item>
                         </SubMenu>
                         <SubMenu
                             key="/app/form"
-                            title={<span><Icon type="scan" /><span className="nav-text">Form</span></span>}
+                            title={<span><Icon type="scan"/><span className="nav-text">Form</span></span>}
                         >
-                            <Menu.Item key="/app/form/inputtest"><Link to={'/app/form/inputtest'}>Input</Link></Menu.Item>
+                            <Menu.Item key="/app/form/inputtest"><Link
+                                to={'/app/form/inputtest'}>Input</Link></Menu.Item>
                         </SubMenu>
                         <SubMenu
                             key="/app/feedback"
-                            title={<span><Icon type="scan" /><span className="nav-text">Feedback</span></span>}
+                            title={<span><Icon type="scan"/><span className="nav-text">Feedback</span></span>}
                         >
-                            <Menu.Item key="/app/feedback/alerttest"><Link to={'/app/feedback/alerttest'}>Alert</Link></Menu.Item>
+                            <Menu.Item key="/app/feedback/alerttest"><Link
+                                to={'/app/feedback/alerttest'}>Alert</Link></Menu.Item>
                         </SubMenu>
                         <Menu.Item key="3">
-                            <Icon type="upload" />
+                            <Icon type="upload"/>
                             <span className="nav-text">nav 3</span>
                         </Menu.Item>
                     </Menu>
                 </Sider>
                 <Layout>
-                    <Header style={{ background: '#fff', padding: '0 10px' }}>
+                    <Header style={{background: '#fff', padding: '0 10px'}}>
                         <Icon
                             className="trigger"
                             type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
                             onClick={this.toggle}
                         />
                     </Header>
-                    <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
+                    <Content style={{margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280}}>
                         {this.props.children}
                     </Content>
                 </Layout>
--- backpage/src/index.js
+++ backpage/src/index.js
@@ -11,25 +11,25 @@ import InputTest from './components/form/InputTest';
 import AlertTest from './components/feedback/AlertTest';
 
 var routes =
-<Route path={'/'} components={Page}>
-    <IndexRedirect to="/app/ui/buttons" />
-    <Route path={'app'} component={App}>
-    <Route path={'ui'}>
-    <Route path={'buttons'} component = {Buttons}/>
-    <Route path={'grids'} component = {Grids}/>
-    </Route>
-    <Route path={'form'}>
-    <Route path={'inputtest'} component = {InputTest}/>
-    </Route>
-    <Route path={'feedback'}>
-    <Route path={'alerttest'} component = {AlertTest}/>
-    </Route>
-    </Route>
+    <Route path={'/'} components={Page}>
+        <IndexRedirect to="/app/ui/buttons"/>
+        <Route path={'app'} component={App}>
+            <Route path={'ui'}>
+                <Route path={'buttons'} component={Buttons}/>
+                <Route path={'grids'} component={Grids}/>
+            </Route>
+            <Route path={'form'}>
+                <Route path={'inputtest'} component={InputTest}/>
+            </Route>
+            <Route path={'feedback'}>
+                <Route path={'alerttest'} component={AlertTest}/>
+            </Route>
+        </Route>
     </Route>
 
-    ReactDOM.render(
+ReactDOM.render(
     <Router history={hashHistory}>
-    {routes}
+        {routes}
     </Router>,
     document.getElementById('root'));
 

a7f48e6ab36afe44c13ce15f609573e23cf3f2df
wyh19 <wyh_19@163.com>
2017-08-01 17:14:09 +0800

    开发dashbord

--- backpage/src/components/app/base/Role.jsx
+++ backpage/src/components/app/base/Role.jsx
@@ -1,3 +1,22 @@
 /**
  * Created by 30113 on 2017/8/1.
  */
+import React, {Component} from 'react';
+import {Tabel} from 'antd';
+
+const data=[
+
+];
+
+class Role extends Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Role;
